4f161f6264552c7d0cdcce37c849f1374a7f14ae
Brandon Troche <bttroche@gmail.com>
2018-01-04 17:21:46 -0500

    Client Routes work and Server End Points work for returning data. Client and Server now communicate

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -2,7 +2,10 @@
   <div class="hello">
       <ul>
       
-        <li><button @click="postL">Hello</button></li>
+          <li><button @click="postL">labs</button></li>
+          <li><button @click="postI">icd</button></li>
+          <li><button @click="postD">diagnosis</button></li>
+          <li><button @click="postP">patients</button></li>
       
       </ul>
   </div>
@@ -25,6 +28,24 @@ export default {
     methods: {
         async postL () {
             const response = await postLabs.queryDB({
+                test: 'test'
+            })
+            console.log(response.data)
+        },
+        async postD () {
+            const response = await postDiagnosis.queryDB({
+                test: 'test'
+            })
+            console.log(response.data)
+        },
+        async postP () {
+            const response = await postPatients.queryDB({
+                test: 'test'
+            })
+            console.log(response.data)
+        },
+        async postI () {
+            const response = await postIcd.queryDB({
                 test: 'test'
             })
             console.log(response.data)
