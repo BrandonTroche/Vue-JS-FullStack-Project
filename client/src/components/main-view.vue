62b50a2a0cd7ac47deaa3787bea1177ad2fbe8b5
Brandon Troche <bttroche@gmail.com>
2018-01-04 23:57:39 -0500

    The code now currently generates another Plot in Plotly.js and reloads the iframe that is showing it. The "/labs" endpoint calculates the most frequent 10 diagnosis and will plot those into a second graph on poorly and that will be displayed under the first plot. ^Once the above is completed, the project will be complete. *** NOTE :: DEVELOPMENT IS AT A HALT FOR TESTING PLOTLY ::: REACHED 100 CHART LIMIT ***

--- client/src/components/main-view.vue
+++ client/src/components/main-view.vue
@@ -1,16 +1,5 @@
 <template>
     <div>
-<!--
-        <ul>
-      
-            <li><button @click="postL">labs</button></li>
-            <li><button @click="postI">icd</button></li>
-            <li><button @click="postD">diagnosis</button></li>
--->
-<!--            <li><button @click="postP">patients</button></li>-->
-            
-      
-<!--        </ul>-->
         <div class="blue">
         <table border="1">
         
@@ -22,16 +11,14 @@
                 <th style="background-color:#a6b1c1;">Sex</th>
             </tr>
             <tr v-for="pa in patient">
-<!--                <li >-->
                 
-                    <th @click="postD(pa.id)">{{pa.id}}</th>
+                    <th @click="postD(pa.id); postL(pa.id)">{{pa.id}}</th>
                 
-                    <th @click="postD(pa.id)">{{pa.risk_score}}</th>
+                    <th @click="postD(pa.id); postL(pa.id)">{{pa.risk_score}}</th>
                 
-                    <th @click="postD(pa.id)">{{pa.sex}}</th>
+                    <th @click="postD(pa.id); postL(pa.id)">{{pa.sex}}</th>
                 
-                    <th @click="postD(pa.id)">{{pa.age}}</th>
-<!--                </li>-->
+                    <th @click="postD(pa.id); postL(pa.id)">{{pa.age}}</th>
             
             </tr>
             
@@ -39,7 +26,7 @@
         </div>
         <div class="yellow">
             
-            a
+            <iframe id="plot1" src="https://plot.ly/~btroche/3.embed" frameborder="0" style="width:100%;height:75%;"></iframe>
         
         </div>
         <div class="red">
@@ -61,13 +48,7 @@
                     <th>{{dia.DESCRIPTION}}</th>
             
             </tr>
-<!--
-                
-                <li v-for="dat in diagnosis.DATE" style="float:left">DATE: {{dat}}</li>
-                <li v-for="icd in diagnosis.ICD" style="float:left">ICD: {{icd}}</li>
-                <li v-for="des in diagnosis.DESCRIPTION" style="float:left">DESCRIPTION: {{des}}</li>
-                            
--->
+            
         </table>
         </div>
     </div>
@@ -92,11 +73,12 @@ export default {
         }
     },
     methods: {
-        async postL () {
-            const response = await postLabs.queryDB({
-                test: 'test'
+        postL (patient) {
+            const response = postLabs.queryDB({
+                patient_id: patient
             })
-            console.log(response.data)
+            
+            document.getElementById('plot1').src += ''
         },
         async postD (patient) {
             const response = await postDiagnosis.queryDB({
@@ -104,23 +86,6 @@ export default {
             })
             
             this.diagnosis = response.data
-//            console.log(response.data)
-//            console.log(patient)
-//            console.log(this.dataset.i)
-        },
-        async postP () {
-            const response = await postPatients.queryDB({
-                test: 'test'
-            })
-//            this.patient.push(response.data)
-            this.patient = response.data
-//            console.log(response.data)
-        },
-        async postI () {
-            const response = await postIcd.queryDB({
-                test: 'test'
-            })
-            console.log(response.data)
         }
     },
     created: async function(){
@@ -145,7 +110,7 @@ export default {
     }
     
     .yellow{
-        background-color: yellow;
+/*        background-color: yellow;*/
         height: 375px !important;
         width: 75% !important;
         float:left;
--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 
 var contents = fs.readFileSync("db.json");
 var jsonContent = JSON.parse(contents);
-console.log(jsonContent.labs[0].lab)
+//console.log(jsonContent.labs[0].lab)
 
 const app = express()
 const plotly = require('plotly')('btroche', '3HB9eLk90oduXc1GJnIb')
@@ -15,9 +15,158 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/labs', (req, res) => {
-    res.send({
-        message:`labs workded`
-    })
+    var values = []
+    var dates = []
+    var values2 = []
+    var dates2 = []
+    
+    for (var i = 0; i < jsonContent.labs.length; i++){
+        
+        if(jsonContent.labs[i].patient_id == req.body.patient_id){
+            values.push(jsonContent.labs[i].value)
+            dates.push(jsonContent.labs[i].date.slice(0,4))
+        }
+
+    }
+    
+    var data = [{x:dates, y:values, type: 'bar'}];
+    
+    var layout = {
+        fileopt : "overwrite", 
+        filename : "valueOverTime",
+        layout: {
+            xaxis: {tickfont: {
+                size: 14,
+                color: "rgb(107, 107, 107)"
+            }},
+            yaxis: {
+                title: "Lab Values",
+                titlefont: {
+                    size: 16,
+                    color: "rgb(107, 107, 107)"
+                },
+                tickfont: {
+                    size: 14,
+                    color: "rgb(107, 107, 107)"
+                }
+            }
+        }
+    };
+    
+    var icd_code = []
+    var date = []
+    var description = []
+    
+    var queryResult = []
+    
+    for (var i = 0; i < jsonContent.diagnosis.length; i++){
+        if (jsonContent.diagnosis[i].patient_id == req.body.patient_id){
+            
+            icd_code.push(jsonContent.diagnosis[i].icd_code_id)
+            date.push(jsonContent.diagnosis[i].date)
+            
+        }
+    }
+    
+    for (var i = 0; i < icd_code.length; i++){
+        for (var j = 0; j < jsonContent.icd_codes.length; j++){
+            if (icd_code[i] == jsonContent.icd_codes[j].id){
+                description.push(jsonContent.icd_codes[j].description)
+                                    
+            }
+        }
+    }
+    
+    for (var i = 0; i < icd_code.length; i++){
+        var myObject = {
+            DATE: date[i],
+            ICD: icd_code[i],
+            DESCRIPTION: description[i]
+        }
+        queryResult.push(myObject)
+    }
+    
+    console.log(queryResult.length)
+    
+    var diagnosisFreqKeys = []
+    var diagnosisFreqValues = []
+    
+    function checkArray(icdValue){
+        for(var j = 0; j < diagnosisFreqKeys.length; j++){
+            if(diagnosisFreqKeys[j] == icdValue){
+                diagnosisFreqValues[j] += 1
+                return true
+            }
+        }
+        return false
+    }
+    
+    for (var i = 0; i < queryResult.length; i++){
+        if(diagnosisFreqKeys.length == 0){
+            diagnosisFreqKeys.push(queryResult[i].ICD)
+            diagnosisFreqValues.push(1)
+        } else {
+             if(!checkArray(queryResult[i].ICD)) {
+                    diagnosisFreqKeys[i] = queryResult[i].ICD
+                    diagnosisFreqValues[i] = 1
+                }
+            }
+        } 
+    
+    var sortedKeys = ['','','','','','','','','','']
+    var sortedArray = [0,0,0,0,0,0,0,0,0,0]
+    
+    for(var i = 0; i < 10; i++){
+        console.log(i)
+        for(var j = 0; j < diagnosisFreqValues.length; j++){
+            console.log(j)
+            console.log(diagnosisFreqValues[j])
+            console.log(sortedArray[i])
+
+            if(diagnosisFreqValues[j] > sortedArray[i]){
+                sortedArray[i] = diagnosisFreqValues[j]
+                sortedKeys[i] = diagnosisFreqKeys[j]
+                diagnosisFreqKeys.splice(j,1)
+                diagnosisFreqValues.splice(j,1)
+            }
+        }
+    }
+    
+    console.log(sortedKeys)
+    console.log(sortedArray)
+//    var data2 = [{x:dates, y:values, type: 'bar'}];
+//    
+//    var layout2 = {
+//        fileopt : "overwrite", 
+//        filename : "diagnosisOverTime",
+//        layout: {
+//            xaxis: {tickfont: {
+//                size: 14,
+//                color: "rgb(107, 107, 107)"
+//            }},
+//            yaxis: {
+//                title: "Diagnosis",
+//                titlefont: {
+//                    size: 16,
+//                    color: "rgb(107, 107, 107)"
+//                },
+//                tickfont: {
+//                    size: 14,
+//                    color: "rgb(107, 107, 107)"
+//                }
+//            }
+//        }
+//    };
+
+    plotly.plot(data, layout, function (err, msg) {
+        if (err) return console.log(err);
+        console.log(msg);
+    });  
+    
+//    plotly.plot(data2, layout2, function (err, msg) {
+//        if (err) return console.log(err);
+//        console.log(msg);
+//    });  
 })
 
 app.post('/diagnosis', (req, res) => {
@@ -57,15 +206,7 @@ app.post('/diagnosis', (req, res) => {
     
     
     res.send(queryResult)
-    
-    
-    
-//    res.send({
-//        id: 0,
-//        patient_id: `NotFound`,
-//        icd_code_id: 0,
-//        date: `NotFound`
-//    })
+
 })
 
 app.post('/patients', (req, res) => {
@@ -80,19 +221,11 @@ app.post('/icd_codes', (req, res) => {
     })
 })
 
-
 app.get('/', (req, res) => {
     res.send({
         message:'hello world!'
     })
 })
 
-//var data = [{x:[0,1,2], y:[3,2,1], type: 'bar'}];
-//var layout = {fileopt : "overwrite", filename : "simple-node-example"};
-//
-//plotly.plot(data, layout, function (err, msg) {
-//	if (err) return console.log(err);
-//	console.log(msg);
-//});
 
 app.listen(process.env.PORT || 3001)
