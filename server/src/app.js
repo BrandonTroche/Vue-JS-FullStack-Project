cb178600b011b7480238f43ca65fa3858529828f
Brandon Troche <bttroche@gmail.com>
2018-01-28 15:51:06 -0500

    no message

--- server/src/app.js
+++ server/src/app.js
@@ -13,8 +13,7 @@ const jsonQuery = require('json-query')
 //console.log(jsonContent.labs[0].lab)
 
 const app = express()
-//const plotly = require('plotly')('btroche', '3HB9eLk90oduXc1GJnIb')
-const plotly = require('plotly')('Btown', 'uJe6Ne1LnU8Pxtit3Fsg')
+const plotly = INSERT_PLOTLY_DETAILS
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
